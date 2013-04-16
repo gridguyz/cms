9421c83a68d91402d99a5e3836cf3b2af371f941
David Pozsar <david.pozsar@megaweb.hu>
2013-04-16 16:07:24 +0200

    initial commit

---
+++ config/languages.php
@@ -0,0 +1,33 @@
+<?php
+
+return array(
+    'default'   => 'en',
+    'available' => array(
+     // english
+        'en'    => true,
+        'en_US' => true,
+        'en_GB' => true,
+        'en_AU' => true,
+        'en_CA' => true,
+        'en_IE' => true,
+     // german
+        'de'    => true,
+        'de_DE' => true,
+        'de_AT' => true,
+        'de_CH' => true,
+        'de_BE' => true,
+     // french
+        'fr'    => true,
+        'fr_FR' => true,
+        'fr_CA' => true,
+        'fr_CH' => true,
+        'fr_BE' => true,
+     // spanish
+        'es'    => true,
+        'es_ES' => true,
+        'es_MX' => true,
+        'es_AR' => true,
+     // hungarian
+        'hu'    => true,
+    ),
+);
