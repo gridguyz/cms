0a647b47f18404f55f295b22cc46b6be84172cc0
David Pozsar <david.pozsar@megaweb.hu>
2013-04-19 13:22:02 +0200

    basic patcher added

---
+++ public/app/patcher.php
@@ -0,0 +1,37 @@
+<?php
+
+use Zork\Patcher\Patcher;
+use Zork\Iterator\CallbackMapIterator;
+
+include './init.php';
+
+$paths = new CallbackMapIterator(
+    new CallbackFilterIterator(
+        new RecursiveIteratorIterator(
+            new RecursiveCallbackFilterIterator(
+                new RecursiveDirectoryIterator(
+                    './vendor',
+                    RecursiveDirectoryIterator::CURRENT_AS_SELF |
+                    RecursiveDirectoryIterator::KEY_AS_FILENAME |
+                    RecursiveDirectoryIterator::SKIP_DOTS |
+                    RecursiveDirectoryIterator::UNIX_PATHS
+                ),
+                function ( $current, $key ) {
+                    return '.' !== $key[0];
+                }
+            ),
+            RecursiveIteratorIterator::SELF_FIRST
+        ),
+        function ( $current, $key ) {
+            return $current->isDir() && $key === 'sql' &&
+                   preg_match( '#[^/]+/[^/]+/(module/[^/]+/)?sql$#', $current->getSubPathname() );
+        }
+    ),
+    function ( $current ) {
+        return './vendor/' . $current->getSubPathname();
+    },
+    CallbackMapIterator::FLAG_GENERATE_KEYS
+);
+
+$patcher = new Patcher( require './config/autoload/db.local.php' );
+$patcher->patch( $paths );
