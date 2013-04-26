e9cbf8cb1715f5d0e718aa6811c89e26fde3549d
David Pozsar <david.pozsar@megaweb.hu>
2013-04-26 11:39:35 +0200

    Multisite install, patch

--- config/application.php
+++ config/application.php
@@ -1,40 +1,40 @@
 <?php
 
-$db = require __DIR__ . '/autoload/db.local.php';
-
-return array(
-    'db'        => $db['db'],
-    'modules'   => array(
-        'Grid\Core',
-        'Grid\Mail',
-        'Grid\Customize',
-        'Grid\Paragraph',
-        'Grid\Tag',
-        'Grid\Image',
-        'Grid\User',
-        'Grid\Menu',
+return array_merge(
+    array(
+        'modules'   => array(
+            'Grid\Core',
+            'Grid\Mail',
+            'Grid\Customize',
+            'Grid\Paragraph',
+            'Grid\Tag',
+            'Grid\Image',
+            'Grid\User',
+            'Grid\Menu',
+        ),
+        'module_listener_options'   => array(
+            'config_glob_paths'     => array(
+                'config/autoload/{,*.}{global,local}.php',
+            ),
+            'module_paths'  => array(
+                'module',
+                'vendor',
+            ),
+        ),
+        'service_manager'   => array(
+            'factories'     => array(
+                'DbAdapter'     => 'Zork\Db\Adapter\AdapterServiceFactory',
+                'ModuleManager' => 'Zork\Mvc\Service\ModuleManagerFactory',
+            ),
+            'invokables'    => array(
+                'SiteConfiguration' => 'Zork\Db\SiteConfiguration\Singlesite',
+            ),
+            'aliases'       => array(
+                'Zork\Db\SiteInfo'                      => 'SiteInfo',
+                'Zend\Db\Adapter\Adapter'               => 'DbAdapter',
+                'Zork\Db\SiteConfigurationInterface'    => 'SiteConfiguration',
+            ),
+        ),
     ),
-    'module_listener_options'   => array(
-        'config_glob_paths'     => array(
-            'config/autoload/{,*.}{global,local}.php',
-        ),
-        'module_paths'  => array(
-            'module',
-            'vendor',
-        ),
-    ),
-    'service_manager'   => array(
-        'factories'     => array(
-            'DbAdapter'     => 'Zork\Db\Adapter\AdapterServiceFactory',
-            'ModuleManager' => 'Zork\Mvc\Service\ModuleManagerFactory',
-        ),
-        'invokables'    => array(
-            'SiteConfiguration' => 'Zork\Db\SiteConfiguration\Singlesite',
-        ),
-        'aliases'       => array(
-            'Zork\Db\SiteInfo'                      => 'SiteInfo',
-            'Zend\Db\Adapter\Adapter'               => 'DbAdapter',
-            'Zork\Db\SiteConfigurationInterface'    => 'SiteConfiguration',
-        ),
-    ),
+    require __DIR__ . '/autoload/db.local.php'
 );
