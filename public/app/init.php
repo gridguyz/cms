f9fd3ae823bd7a5d7f874c1aae31da2551d40aff
David Pozsar <david.pozsar@megaweb.hu>
2013-08-15 10:24:45 +0200

    update public/app

--- public/app/init.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * This makes our life easier when dealing with paths. Everything is relative
- * to the application root now.
- */
-chdir( dirname( dirname( __DIR__ ) ) );
-
-// Setup autoloading
-include 'vendor/autoload.php';
