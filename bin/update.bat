b3190c2220a2618c30fa1a5de96e300759ddb689
David Pozsar <david.pozsar@megaweb>
2013-11-11 12:27:14 +0100

    update script now recognize some composer flags (windows only)

--- bin/update.bat
+++ bin/update.bat
@@ -2,6 +2,19 @@
 CD %~dp0
 CD ..
 
+SET DEVFLAG=--no-dev
+SET PREFLAG=
+SET OPTFLAG=
+
+FOR %%F IN (%*) DO (
+    IF "%%~F"=="--dev"                  SET DEVFLAG=--dev
+    IF "%%~F"=="--no-dev"               SET DEVFLAG=--no-dev
+    IF "%%~F"=="--prefer-dist"          SET PREFLAG=--prefer-dist
+    IF "%%~F"=="--prefer-source"        SET PREFLAG=--prefer-source
+    IF "%%~F"=="-o"                     SET OPTFLAG=--optimize-autoloader
+    IF "%%~F"=="--optimize-autoloader"  SET OPTFLAG=--optimize-autoloader
+)
+
 ECHO.
 ECHO Maintenance activate
 ECHO --------------------
@@ -21,14 +34,17 @@ ECHO Composer update module installer
 ECHO --------------------------------
 ECHO.
 
-php composer.phar update gridguyz/module-installer --no-dev
+SET FLAGS=%DEVFLAG%
+IF NOT "%PREFLAG%"=="" SET FLAGS=%FLAGS% %PREFLAG%
+php composer.phar update gridguyz/module-installer %FLAGS%
 
 ECHO.
 ECHO Composer update
 ECHO ---------------
 ECHO.
 
-php composer.phar update --no-dev
+IF NOT "%OPTFLAG%"=="" SET FLAGS=%FLAGS% %OPTFLAG%
+php composer.phar update %FLAGS%
 
 ECHO.
 ECHO Maintenance deactivate
